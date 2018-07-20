e2de4b5a4f85f49efaa7394e0a280cc45df51e18
Younes Charfaoui <mxcsyounes@ggmail.com>
2018-07-20 15:22:08 +0100

    Adding Comment in files #2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Blockchain
+# Blockchain basic implementaion
 Set of basic implementation of blockchain in python and Kotlin demostrating it's property.
+
+Introduction
+In this project I make the theory and the practice of what I learned in the Blockchain Courses Provided by Udemy and IBM into
+a guideline how to make simple blockchain and it's derivatives.
--- python/1_basic_blockchain/blockchain.py
+++ python/1_basic_blockchain/blockchain.py
@@ -1,12 +1,26 @@
+"""Copyright [2018] [Charfaoui Younes]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."""
+
 # Module 1 - Create a Blockchain
 
-#importing the libraries
+#importing the libraries.
 import datetime
 import hashlib
 import json
 from flask import Flask , jsonify
 
-# Part 1 - Building a Blockchain
+# Building The Blockchain Architecture.
 class Blockchain:
     
     def __init__(self):
@@ -25,6 +39,7 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
     
+    # this the main thing about the blockchain, it's define the nonce to use in actual block that satisfay the current target 
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
@@ -37,10 +52,12 @@ class Blockchain:
                 new_proof+=1
         return new_proof
     
+    # helper function to hash a block
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
+    # helper function to check if the blockchain is valid
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
@@ -57,15 +74,15 @@ class Blockchain:
             block_index += 1
         return True
 
-# Part 2 - Mining our Blockchain
+# The Implementation in the web app.
 
-# Creating web app
+# Creating web app.
 app = Flask(__name__)
    
-# Create a blockchain
+# Create a blockchain.
 blockchain = Blockchain()
 
-#Mining a block
+# Mining a block.
 @app.route('/mine_block', methods = ['GET'])
 def mine_block():
     previous_block  = blockchain.get_previous_block()
@@ -80,13 +97,14 @@ def mine_block():
                 'previous_hash': block['previous_hash']}
     return jsonify(response) , 200
 
-# Getting Full blockchain
+# Getting Full blockchain.
 @app.route('/get_chain',methods = ['GET'])
 def get_chain():
     response = {'chain' : blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+# Checking if the chain is valid via the helper function.
 @app.route('/is_valid', methods = ['GET'])
 def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
@@ -98,6 +116,3 @@ def is_valid():
 
 #Running the app
 app.run(host = '0.0.0.0' , port = 5000)
-
-
-
